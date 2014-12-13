30c868e2d83f070ef8863a6c70645e33a049b633
longsube <longsube@gmail.com>
2014-12-13 21:42:35 +0700

    Up Script

---
+++ control-4.keystone.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -ex
+#
+# Khoi tao bien
+# TOKEN_PASS=a
+# MYSQL_PASS=a
+# ADMIN_PASS=a
+source config.cfg
+
+echo "##### Cai dat keystone #####"
+apt-get install keystone python-keystoneclient -y
+
+#/* Sao luu truoc khi sua file nova.conf
+filekeystone=/etc/keystone/keystone.conf
+test -f $filekeystone.orig || cp $filekeystone $filekeystone.orig
+
+#Chen noi dung file /etc/keystone/keystone.conf
+cat << EOF > $filekeystone
+[DEFAULT]
+admin_token = $TOKEN_PASS
+#log_dir = /var/log/keystone
+verbose = True
+[assignment]
+[auth]
+[cache]
+[catalog]
+[credential]
+[database]
+connection = mysql://keystone:$ADMIN_PASS@controller/keystone
+[ec2]
+[endpoint_filter]
+[federation]
+[identity]
+[kvs]
+[ldap]
+[matchmaker_ring]
+[memcache]
+[oauth1]
+[os_inherit]
+[paste_deploy]
+[policy]
+[revoke]
+[signing]
+[ssl]
+[stats]
+[token]
+provider = keystone.token.providers.uuid.Provider
+driver = keystone.token.persistence.backends.sql.Token
+[trust]
+[extra_headers]
+Distribution = Ubuntu
+EOF
+#
+echo "##### Xoa DB mac dinh #####"
+rm  /var/lib/keystone/keystone.db
+
+echo "##### Khoi dong lai MYSQL #####"
+service keystone restart
+sleep 3
+service keystone restart
+
+echo "##### Dong bo cac bang trong DB #####"
+sleep 3
+keystone-manage db_sync
+
+(crontab -l -u keystone 2>&1 | grep -q token_flush) || \
+echo '@hourly /usr/bin/keystone-manage token_flush >/var/log/keystone/keystone-tokenflush.log 2>&1' >> /var/spool/cron/crontabs/keystone
---
+++ control-horizon.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -ex
+
+source config.cfg
+
+###################
+echo "########## CAI DAT DASHBOARD ##########"
+###################
+sleep 5
+
+echo "########## Cài đặt Dashboard ##########"
+apt-get install openstack-dashboard apache2 libapache2-mod-ws memcached python-memcache -y
+
+echo "########## Cau hinh fix loi cho apache2 ##########"
+sleep 5
+# Fix loi apache cho ubuntu 14.04
+# echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf
+# sudo a2enconf servername 
+echo "ServerName localhost" >> /etc/apache2/httpd.conf
+
+
+echo "########## Tao trang redirect ##########"
+
+filehtml=/var/www/index.html
+test -f $filehtml.orig || cp $filehtml $filehtml.orig
+rm $filehtml
+touch $filehtml
+cat << EOF >> $filehtml
+<html>
+<head>
+<META HTTP-EQUIV="Refresh" Content="0.5; URL=http://$CON_EXT_IP/horizon">
+</head>
+<body>
+<center> <h1>Dang chuyen den Dashboard cua OpenStack</h1> </center>
+</body>
+</html>
+EOF
+
+sed -i 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "controller"/g' /etc/openstack-dashboard/local_settings.py
+sed -i "s/#ALLOWED_HOSTS = ['horizon.example.com', ]/ALLOWED_HOSTS = ['*']/g" /etc/openstack-dashboard/local_settings.py
+
+# Cho phep chen password tren dashboad ( chi ap dung voi image tu dong )
+sed -i "s/'can_set_password': False/'can_set_password': True/g" /etc/openstack-dashboard/local_settings.py
+#sed -i "s/'enable_vpn': False/'enable_vpn': True/g" /etc/openstack-dashboard/local_settings.py
+sed -i "s/'enable_lb': False/'enable_lb': True/g" /etc/openstack-dashboard/local_settings.py
+
+## /* Khởi động lại apache và memcached
+service apache2 restart
+service memcached restart
+echo "########## Hoan thanh cai dat Horizon ##########"
+
+echo "########## THONG TIN DANG NHAP VAO HORIZON ##########"
+echo "URL: http://$CON_EXT_IP/horizon"
+echo "User: admin hoac demo"
+echo "Password:" $ADMIN_PASS
